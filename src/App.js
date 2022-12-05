1d9c9eabadf3ef2c0a6e228cba04d535ed13ad00
Devvicktor <kosgeivick90@gmail.com>
2022-12-05 08:07:36 +0300

    passed props as object to MovieBox component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieBox from './MovieBox';
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieBox from "./MovieBox";
 
 function App() {
-  const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=9fc0b4db7010ebe15f369cba0cf8d13a";
-  const [movies,setMovies]=useState([]);
+  const API_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key=9fc0b4db7010ebe15f369cba0cf8d13a";
+  const [movies, setMovies] = useState([]);
 
-   useEffect(()=>{
-     fetch(API_URL)
-     .then((res)=>res.json())
-     .then(data=>{
-      console.log(data);
-      setMovies(data.results);
-    })
-   },[])
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setMovies(data.results);
+      });
+  }, []);
 
   return (
     <div>
       <h3>Display movies here now</h3>
-      {movies.map((movieReq)=><MovieBox key={movieReq.id}{...movieReq}/>)}
+      {movies.map((movieReq) => (
+        <MovieBox key={movieReq.id} {...movieReq} />
+      ))}
     </div>
   );
 }
--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,13 +1,19 @@
-import React from 'react'
- const API_IMAGE="https://image.tmdb.org/t/p/w500/"
-const MovieBox = (title,poster_path,vote_average,release_date,overview) => {
+import React from "react";
+const API_IMAGE = "https://image.tmdb.org/t/p/w500/";
+const MovieBox = ({
+  title,
+  poster_path,
+  vote_average,
+  release_date,
+  overview,
+}) => {
   return (
     <div>
       <h1>{title}</h1>
-      <img src={API_IMAGE+poster_path} alt="not found"></img>
+      <img src={API_IMAGE + poster_path} alt='not found'></img>
       <p>{overview}</p>
     </div>
-  )
-}
+  );
+};
 
-export default MovieBox
+export default MovieBox;
